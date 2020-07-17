6aab63311561c0417604f14b767c60676c2d4711
Richard Barbsosa <richardbarbosa017@gmail.com>
2020-07-17 13:47:40 -0300

    Tarefa 2

--- Task2/Task2.js
+++ Task2/Task2.js
@@ -4,8 +4,30 @@
  * @return {string}
  */
 const replaceChar = (string, times) => {
-    //Your logic goes here
-    return null;
+    var str = string.split('');
+    var aux = 1;
+    for(var i = 0; i<str.length; i++){
+        if(i == 0 && str[i] == '?'){
+            while(str[str.length-aux] == '?'){
+                aux++;
+            }
+            str[i] = str[str.length-aux];
+            for (var j = 0;j<times-1;j++){
+                str[i] += str[str.length-aux];
+            }
+            aux=1;
+        } else if(str[i] == '?'){
+            while(str[i-aux] == '?'){
+                aux++;
+            }
+            str[i] = str[i-aux];
+            for (var j = 0;j<times-1;j++){
+                str[i] += str[i-aux];
+            }
+            aux=1;
+        } 
+    } 
+    return str.join("");
    }
    export default replaceChar;
    
