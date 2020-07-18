b8d9d71a374b958e3e23d3010a7f5aa52b368e58
Richard Barbsosa <richardbarbosa017@gmail.com>
2020-07-18 17:52:46 -0300

    Tarefa 3

--- Task3/Task3.js
+++ Task3/Task3.js
@@ -4,7 +4,15 @@
 * @return {number}
 */
 const lettersSum = (string1, string2) => {
-//Your logic goes here
-return null;
+    var str1 = string1.split('').sort();
+    var str2 = string2.split('').sort();
+    var bigger1 = str1[str1.length-1].charCodeAt(0)-96;
+    var smaller1 = str1[0].charCodeAt(0)-96;
+    var bigger2 = str2[str2.length-1].charCodeAt(0)-96;
+    var smaller2 = str2[0].charCodeAt(0)-96;
+
+    var ret = (bigger1*smaller2)+(bigger2*smaller1);
+
+    return ret;
 }
 export default lettersSum;
